2de7aeca496fe80368ab2faf6fe027b63a4b6cda
Pablo Silva <pablo.juninho2018@gmail.com>
2024-11-25 01:38:32 -0300

    Bumpin' That

--- bumpin_that.py
+++ bumpin_that.py
@@ -10,10 +10,10 @@ async def initialize(client, channel):
 
     async def wrapper():
         while True:
-            print("BUMPING THAT")
-            await client.get_channel(channel).send("https://cdn.discordapp.com/attachments/637679343740649512/1310461793294094356/Sem_titulo.png?ex=67454e26&is=6743fca6&hm=83f55dfdf8ea755af978556c41f1e5e6d59e7f32af9cbebb3a2b060227ccd529&")
             print("WAITING FOUR MORE HOURS FOR BUMP")
             await asyncio.sleep(FOUR_HOURS)
+            print("BUMPING THAT")
+            await client.get_channel(channel).send("https://cdn.discordapp.com/attachments/637679343740649512/1310461793294094356/Sem_titulo.png?ex=67454e26&is=6743fca6&hm=83f55dfdf8ea755af978556c41f1e5e6d59e7f32af9cbebb3a2b060227ccd529&")
 
-    await asyncio.create_task(wrapper())
+    asyncio.create_task(wrapper())
     print("BUMP MODULE INITIALIZED SUCCESSFULLY")
