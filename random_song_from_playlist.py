b64e78f22403dbf87b85e4bcfae87b84bf2899ea
Pablo Silva <pablo.juninho2018@gmail.com>
2024-11-25 01:47:34 -0300

    Bumpin' That

--- random_song_from_playlist.py
+++ random_song_from_playlist.py
@@ -122,7 +122,9 @@ async def initialize(client, youtube_api_token):
 
     while True:
         now = datetime.now()
-        next_run = datetime.combine(now.date(), datetime.min.time()) + timedelta(days=1, hours=12)
+        next_run = datetime.combine(now.date(), datetime.min.time()) + timedelta(hours=12)
+        if now >= next_run:
+            next_run += timedelta(days=1)
         sleep_duration = (next_run - now).total_seconds()
         print(f"Sleeping for {sleep_duration} seconds until next random song at {next_run}")
         await asyncio.sleep(sleep_duration)
